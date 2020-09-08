85e1b1d584bff61705fe4ccba0534dc95ac0dde2
Francisco Rodrigues <chicofariasneto@gmail.com>
2020-09-08 16:49:48 -0300

    style(index): add semicolon and comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = process.env.PORT
+require('dotenv').config(); // try to use console.log(require('dotenv')); to see if you file is ok. Or you can use { path: 'path .env' }
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -10,7 +12,7 @@ app.use(
     bodyParser.urlencoded({
         extended: true,
     })
-)
+);
 
 app.get('/', (request, response) => {
     response.json({
@@ -18,11 +20,11 @@ app.get('/', (request, response) => {
         Obs: '...',
         Documentation: '...',
         Port: `APP running on port ${port}.`
-    })
-})
+    });
+});
 
-require('./app/controller/index')(app)
+require('./app/controller/index')(app);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+    console.log(`App running on port ${port}.`);
 });
